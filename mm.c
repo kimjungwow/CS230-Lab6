e4da4df5a38ce428ab6c79a3d086c617f491f92a
KimJungwoo <jw950310@kaist.ac.kr>
2018-11-26 23:39:43 +0900

    first

--- mm.c
+++ mm.c
@@ -18,20 +18,376 @@
 #include "mm.h"
 #include "memlib.h"
 
-/* single word (4) or double word (8) alignment */
+/* TEXTBOOK START */
+
+/* Basic constants and macros */
+#define WSIZE 4
+#define DSIZE 8
+#define CHUNKSIZE (1 << 12)
+
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
+#define evendown(x) (x % 2 == 1 ? (x - 1) : (x))
+
+/* Pack a size and allocated bit into a word */
+#define PACK(size, alloc) ((size) | (alloc))
+
+/* Read and write a word at address p */
+#define GET(p) (*(unsigned int *)(p))
+#define PUT(p, val) (*(unsigned int *)(p) = (val))
+
+/* Read the size and allocated fields from address p*/
+#define GET_SIZE(p) (GET(p) & -0x7)
+#define GET_ALIGN(p) (GET(p) & 0x4)
+#define GET_ALLOC(p) (GET(p) & 0x1)
+
+/* Given block ptr bp, compute address of its header and footer */
+#define HDRP(bp) ((char *)(bp)-WSIZE)
+#define FTRP(bp) ((char *)(bp) + GET_SIZE(HDRP(bp)) - DSIZE)
+
+/* Given block ptr bp, compute address of next and previous blocks */
+#define NEXT_BLKP(bp) ((char *)(bp) + evendown(GET_SIZE(((char *)(bp)-WSIZE))))
+#define PREV_BLKP(bp) ((char *)(bp)-evendown(GET_SIZE(((char *)(bp)-DSIZE))))
+
+/* TEXTBOOK END*/
+///////////////MINE
+
+#define GET_PREV(p) (*(char **)((char *)(p) + WSIZE))
+#define GET_NEXT(p) (*(char **)((char *)(p)))
+
+#define SET_NEXT(p, next) (*(char **)(p) = (next))
+#define SET_PREV(p, prev) (*(char **)((char *)(p) + WSIZE) = (prev))
+
+#define INC_ALLOC(p) (GET_ALLOC(HDRP(NEXT_BLKP(p))))
+#define DEC_ALLOC(p) (GET_ALLOC(HDRP(PREV_BLKP(p))))
+
+#define INC_PTR(p) ((char *)(p) + GET_SIZE(((char *)(p)-WSIZE)))
+
+///////////////
+
+/*/ single word (4) or double word (8) alignment */
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
-
+#define ALIGN(size) (((size) + (ALIGNMENT - 1)) & ~0x7)
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+/* GLOBAL SCALAR VALUE */
+struct NODE
+{
+    size_t SIZE;
+    struct NODE *NEXT; // Next root with bigger size
+    char *FIRST;       // Pointer to first node in this size
+};
+struct NODE *root1;
+static char *root;
+static struct NODE *rootend;
+
+size_t count_binary(size_t given);
+static struct NODE *find_struct_size(size_t siz);
+static struct NODE *find_struct(void *ptr);
+
+static void *first_fit(size_t siz);
+static void *extend_heap(size_t words);
+
+static void *coalesce(void *bp);
+static void delete_node(char *ptr);
+static void insert_node(char *ptr);
+static void *split(void *bp, size_t siz);
+/* GLOBAL SCALAR VALUE */
+
+size_t count_binary(size_t given)
+{
+    size_t useful = 0;
+    int tempgiven = given;
+    while (tempgiven > 0)
+    {
+        useful++;
+        tempgiven = tempgiven >> 1;
+    }
+    return useful;
+}
+
+static struct NODE *find_struct_size(size_t siz)
+{
+    struct NODE *tempstructsize = root1;
+    do
+    {
+        if (tempstructsize->SIZE <= siz)
+        {
+            return tempstructsize;
+        }
+        else
+        {
+            if (tempstructsize->NEXT == 0)
+            {
+                break;
+            }
+            else
+            {
+                tempstructsize = tempstructsize->NEXT;
+            }
+        }
+
+    } while (tempstructsize != 0);
+    return rootend;
+}
+
+static struct NODE *find_struct(void *ptr)
+{
+    size_t checksize = evendown(GET_SIZE(HDRP(ptr)));
+    size_t ptrsize = 0;
+    while (checksize > 0)
+    {
+        checksize = checksize >> 1;
+        ptrsize += 1;
+    }
+    struct NODE *tempstruct = root1;
+    do
+    {
+        if (tempstruct->SIZE <= ptrsize)
+        {
+            return tempstruct;
+        }
+        else
+        {
+            if (tempstruct->NEXT == 0)
+            {
+                break;
+            }
+            else
+            {
+                tempstruct = tempstruct->NEXT;
+            }
+        }
+
+    } while (tempstruct != 0);
+    return rootend;
+}
+
+static void delete_node(char *ptr)
+{
+    struct NODE *root = find_struct(ptr);
+    char *delete_temp = root->FIRST;
+    while (1)
+    {
+        if (delete_temp == ptr)
+        {
+            if (delete_temp == root->FIRST)
+            {
+                if (GET_NEXT(ptr) != 0)
+                {
+                    root->FIRST = GET_NEXT(ptr);
+                    SET_PREV(GET_NEXT(ptr), NULL);
+                }
+                else
+                {
+                    root->FIRST = NULL;
+                }
+            }
+            else
+            {
+                if (GET_NEXT(ptr) != 0)
+                {
+                    SET_PREV(GET_NEXT(ptr), GET_PREV(ptr));
+                    SET_NEXT(GET_PREV(ptr), GET_NEXT(ptr));
+                }
+                else
+                {
+                    SET_NEXT(GET_PREV(ptr), NULL);
+                }
+            }
+            SET_PREV(ptr, NULL);
+            SET_NEXT(ptr, NULL);
+            return;
+        }
+        else
+        {
+            if ((delete_temp < (char *)(mem_heap_lo())) || (delete_temp > (char *)(mem_heap_hi())))
+            {
+                return;
+            }
+            delete_temp = GET_NEXT(delete_temp);
+        }
+    }
+    return;
+}
+
+static void insert_node(char *ptr)
+{
+    struct NODE *root = find_struct(ptr);
+
+    if (((void *)(root->FIRST)>mem_heap_lo())&&((void *)(root->FIRST)<mem_heap_hi()))
+    {
+        char *b4 = root->FIRST;
+        SET_PREV(b4, ptr);
+        SET_NEXT(ptr, b4);
+        SET_PREV(ptr, NULL);
+    }
+    else
+    {
+        SET_PREV(ptr, NULL);
+        SET_NEXT(ptr, NULL);
+    }
+
+    root->FIRST = ptr;
+
+    return;
+}
+
+static void *extend_heap(size_t words)
+{
+    char *bp;
+    size_t size;
+    size = ((words + 1) & -0x2);
+    size *= WSIZE;
+    if ((long)(bp = mem_sbrk(size)) == -1)
+        return NULL;
+
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
+    PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
+    insert_node(bp);
+
+    return coalesce(bp);
+}
+
+static void *coalesce(void *bp)
+{
+
+    size_t prev_alloc;
+    size_t next_alloc;
+
+    if (GET_SIZE(bp - DSIZE) == 0)
+        prev_alloc = 1;
+    else
+        prev_alloc = GET_ALLOC(bp - DSIZE);
+    if ((NEXT_BLKP(bp) == 0) || ((void *)(NEXT_BLKP(bp)) > mem_heap_hi()))
+        next_alloc = 1;
+    else
+        next_alloc = INC_ALLOC(bp);
+
+    size_t size = GET_SIZE(HDRP(bp));
+
+    if (prev_alloc && next_alloc)
+        return bp;
+    else if (prev_alloc && !next_alloc)
+    {
+        if ((void *)(NEXT_BLKP(bp)) > mem_heap_hi())
+        {
+            return bp;
+        }
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+        size = evendown(size);
+        delete_node(bp);
+        delete_node(NEXT_BLKP(bp));
+
+        PUT(HDRP(bp), PACK(size, 0));
+        PUT(FTRP(bp), PACK(size, 0));
+    }
+    else if (!prev_alloc && next_alloc)
+    {
+
+        size += GET_SIZE(FTRP(PREV_BLKP(bp)));
+        size = evendown(size);
+
+        delete_node(PREV_BLKP(bp));
+        delete_node(bp);
+        PUT(FTRP(bp), PACK(size, 0));
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+
+        bp = PREV_BLKP(bp);
+    }
+    else
+    {
+        size += GET_SIZE(HDRP(NEXT_BLKP(bp))) + GET_SIZE(FTRP(PREV_BLKP(bp)));
+        size = evendown(size);
+        delete_node(PREV_BLKP(bp));
+        delete_node(bp);
+        delete_node(NEXT_BLKP(bp));
+
+        PUT(HDRP(PREV_BLKP(bp)), PACK(size, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(size, 0));
+        bp = PREV_BLKP(bp);
+    }
+    insert_node(bp);
+    return bp;
+}
+
+static void *first_fit(size_t siz)
+{
+    struct NODE *tempforfit = find_struct_size(count_binary(siz));
+    char *tempinstruct = tempforfit->FIRST;
+    while (tempinstruct != 0)
+    {
+        if (GET_SIZE(HDRP(tempinstruct)) >= siz)
+        {
+            return tempinstruct;
+        }
+        else
+        {
+            tempinstruct = GET_NEXT(tempinstruct);
+        }
+    }
+    return NULL;
+}
+
+static void *split(void *bp, size_t siz)
+{
+    if (GET_SIZE(HDRP(bp)) - siz > ALIGNMENT)
+    {
+        int splitsize = GET_SIZE(HDRP(bp)) - siz;
+        delete_node(bp);
+        PUT(HDRP(bp), PACK(siz, 0));
+        PUT(FTRP(bp), PACK(siz, 1));
+        PUT(HDRP(bp), PACK(siz, 1));
+        PUT(HDRP(NEXT_BLKP(bp)), PACK(splitsize, 0));
+        PUT(FTRP(NEXT_BLKP(bp)), PACK(splitsize, 0));
+        insert_node(NEXT_BLKP(bp));
+    }
+    else
+    {
+        delete_node(bp);
+        PUT(FTRP(bp), PACK(siz, 1));
+        PUT(HDRP(bp), PACK(siz, 1));
+    }
+    return bp;
+}
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+
+    if ((root1 = mem_sbrk(120)) == (void *)-1)
+        return -1;
+    memset(root1, 0, 120);
+
+    struct NODE *initroot = root1;
+    int i;
+    for (i = 0; i < 10; i++)
+    {
+        initroot->SIZE = 5 + i;
+        initroot->NEXT = initroot + 1;
+        initroot->FIRST = NULL;
+        initroot++;
+    }
+    rootend = initroot - 1;
+
+    if ((root = mem_sbrk(4 * WSIZE)) == (void *)-1)
+        return -1;
+    memset(root, 0, 16);
+
+    PUT(root, 0);
+    PUT(root + WSIZE, PACK(2 * WSIZE, 1));
+    PUT(root + (2 * WSIZE), PACK(2 * WSIZE, 1));
+    PUT(root + (3 * WSIZE), PACK(0, 1));
+    root += (4 * WSIZE);
+
+    if (extend_heap(CHUNKSIZE) == NULL)
+        return -1;
+
     return 0;
 }
 
@@ -41,13 +397,43 @@ int mm_init(void)
  */
 void *mm_malloc(size_t size)
 {
+    if (size == 0)
+        return NULL;
+
     int newsize = ALIGN(size + SIZE_T_SIZE);
-    void *p = mem_sbrk(newsize);
+    size_t useful = count_binary(newsize);
+
+    if (find_struct_size(useful)->FIRST != 0)
+    {
+        char *temp = first_fit(newsize);
+        if (temp != NULL)
+        {
+
+            return split(temp, newsize);
+        }
+    }
+
+    int extend;
+    if (newsize>CHUNKSIZE)
+        extend=newsize;
+    else
+        extend=CHUNKSIZE;
+
+    void *p = mem_sbrk(extend);
     if (p == (void *)-1)
-	return NULL;
-    else {
-        *(size_t *)p = size;
-        return (void *)((char *)p + SIZE_T_SIZE);
+        return NULL;
+    else
+    {
+
+        PUT(HDRP(p), PACK(newsize, 0));
+        PUT(FTRP(p), PACK(newsize, 1));
+        PUT(HDRP(p), PACK(newsize, 1));
+        if (newsize<CHUNKSIZE) {
+            PUT(HDRP(NEXT_BLKP(p)), PACK(CHUNKSIZE-newsize, 0));
+            PUT(FTRP(NEXT_BLKP(p)), PACK(CHUNKSIZE-newsize, 0));
+            insert_node(NEXT_BLKP(p));
+        }
+        return p;
     }
 }
 
@@ -56,6 +442,17 @@ void *mm_malloc(size_t size)
  */
 void mm_free(void *ptr)
 {
+    size_t size = evendown(GET_SIZE(HDRP(ptr)));
+
+    PUT(HDRP(ptr), PACK(size, 0));
+    PUT(FTRP(ptr), PACK(size, 0));
+    SET_NEXT(ptr, NULL);
+    SET_PREV(ptr, NULL);
+    insert_node(ptr);
+
+    coalesce(ptr);
+
+    return;
 }
 
 /*
@@ -63,31 +460,50 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
-    void *oldptr = ptr;
-    void *newptr;
-    size_t copySize;
-    
-    newptr = mm_malloc(size);
-    if (newptr == NULL)
-      return NULL;
-    copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
-    if (size < copySize)
-      copySize = size;
-    memcpy(newptr, oldptr, copySize);
-    mm_free(oldptr);
-    return newptr;
+    if (ptr == NULL)
+        return mm_malloc(size);
+    if (size == 0)
+    {
+        mm_free(ptr);
+        return NULL;
+    }
+    int newsize = ALIGN(size + SIZE_T_SIZE);
+    size_t diff;
+    if (evendown(GET_SIZE(HDRP(ptr))) >= newsize)
+    {
+        diff = evendown(GET_SIZE(HDRP(ptr))) - newsize;
+        if (diff > ALIGNMENT)
+        {
+            delete_node(ptr);
+            PUT(HDRP(ptr), PACK(newsize, 0));
+            PUT(FTRP(ptr), PACK(newsize, 1));
+            PUT(HDRP(ptr), PACK(newsize, 1));
+            PUT(HDRP(NEXT_BLKP(ptr)), PACK(diff, 0));
+            PUT(FTRP(NEXT_BLKP(ptr)), PACK(diff, 0));
+            insert_node(NEXT_BLKP(ptr));
+            return ptr;
+        }
+        else
+        {
+            return ptr;
+        }
+    }
+    else
+    {
+        void *oldptr = ptr;
+        void *newptr;
+        size_t copySize;
+
+        newptr = mm_malloc(size);
+        if (newptr == NULL)
+            return NULL;
+        copySize = (GET_SIZE(HDRP(oldptr))-SIZE_T_SIZE);
+        // copySize = *(size_t *)((char *)oldptr - SIZE_T_SIZE);
+        if (size < copySize)
+            copySize = size;
+        memcpy(newptr, oldptr, copySize);
+
+        mm_free(oldptr);
+        return newptr;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
